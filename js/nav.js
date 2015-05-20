4444533b050ab1385e52d441516ef3e8b4f66c0b
Jellefish <JelleClignet@MacBook-Pro.local>
2015-05-20 15:01:09 +0200

    push

---
+++ js/nav.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+  $('#menulink').click(function(event) {
+    event.preventDefault();
+    if($('.navigation-wrapper').hasClass('show-menu')) {
+      $('.navigation-wrapper').removeClass('show-menu');
+      $('.navigation').hide();
+      $('.navigation li').removeClass('small-padding');
+    } else {
+      $('.navigation-wrapper').addClass('show-menu');
+      $('.navigation').fadeIn();
+      $('.navigation li').addClass('small-padding');
+     }
+  });
+  
+  $('#menulink1').click(function(event) {
+    event.preventDefault();
+    if($('.navigation-wrapper').hasClass('show-menu')) {
+      $('.navigation-wrapper').removeClass('show-menu');
+      $('.navigation').hide();
+      $('.navigation li').removeClass('small-padding');
+    } else {
+      $('.navigation-wrapper').addClass('show-menu');
+      $('.navigation').fadeIn();
+      $('.navigation li').addClass('small-padding');
+     }
+  });
+});
